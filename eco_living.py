4cb3b31b1a47963aaa86ceb9e5b9a94adc63b47d
LLYX <leon.xu@mail.utoronto.ca>
2016-05-27 17:11:00 -0400

    Change lower bounds for existent exchange reactions of amino acids

--- eco_living.py
+++ eco_living.py
@@ -13,51 +13,7 @@ from os.path import join
 
 # create new model from sbml file
 print("importing the existing the ecoli model...")
-eco = cobra.io.sbml.create_cobra_model_from_sbml_file("/home/zion/Documents/gitStuff/igem/gol_FBA/msb201165-s3.xml",old_sbml=False,legacy_metabolite=False,print_time=False,use_hyphens=False)
-
-'''          ignoring the following block of code
-print("Creating new_model...")
-new_model = Model('new_model')
-
-
-
-print("Adding compartment 'a' for 'all'...")
-new_model.compartments['a'] = 'all'
-
-#Adding all metabolites from existing model removing duplications
-print("Adding all metabolites from existing model removing duplications...")
-for x in eco.metabolites:
-	dup = False
-	for y in new_model.metabolites:
-		if x.id[:-2] == y.id:
-			dup = True
-			break
-	if dup == False:
-		met = copy.deepcopy(x)
-		met.id = met.id[:-2]+'_a'
-		met.compartment = 'a'
-		new_model.add_metabolites({met})
-
-#Adding all reactions excluding transport, exchange, flippase, sink
-print("Adding all reactions excluding transport, exchange, flippase, sink...")
-for react in eco.reactions:
-	if (react.name.find('transport') == -1 and react.subsystem.find('Transport') == -1 and react.name.find('exchange') == -1 and react.name.find('flippase') == -1  and react.name.find('Sink') == -1):
-		for x in react.reactants:
-			x.id = x.id[:-2]+'_a'
-		for x in react.products:
-			x.id = x.id[:-2]+'_a'
-		new_model.add_reaction(react)
-print("Number of reactions in new_model",len(new_model.reactions))
-
-#removing all reactions that are dupliates
-print("Removing all duplicate reactions...")
-for react in new_model.reactions:
-    for y in new_model.reactions:
-        if react.reaction == y.reaction and react.name != y.name:
-            y.delete()
-print("Number of reactions after deleting duplicates: ",len(new_model.reactions))
-#'''
-
+eco = cobra.io.sbml.create_cobra_model_from_sbml_file("/home/xuleon1/Desktop/iGEM/gol_FBA-1/msb201165-s3.xml",old_sbml=False,legacy_metabolite=False,print_time=False,use_hyphens=False)
 
 # adding necessary metabolites and reactions for biosensor project
 print("Adding necessary metabolites and reactions for biosensor project...")
@@ -80,5 +36,21 @@ reaction.add_metabolites({CPRG: -1, H2O: -1, BDGal: 1, CPR: 1})
 
 eco.add_reaction(reaction)
 
-print("Writing to SBML file 'eco_living.xml'...")
-cobra.io.write_sbml_model(eco, "eco_living.xml",sbml_level=2,sbml_version=1,print_time=False,use_fbc_package=False)
+# change lower bounds for exchange reactions specified in KU Leuven 2013
+rxn_to_lb = {}
+
+with open("exchange_rxn_names.txt") as exchange_rxns:
+    for line in exchange_rxns:
+        line = line.rstrip("\n").split(",")
+
+        rxn_to_lb[line[0] + " exchange"] = float(line[1])
+
+raw_input("Press enter to continue...")
+
+for rxn in eco.reactions:
+    if rxn.name in rxn_to_lb:
+	print(rxn.name)
+        rxn.lower_bound = rxn_to_lb[rxn.name]
+
+print("Writing to SBML file 'eco_living2.xml'...")
+cobra.io.write_sbml_model(eco, "eco_living2.xml",sbml_level=2,sbml_version=1,print_time=False,use_fbc_package=False)
